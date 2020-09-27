6c0de47d0612a96567b4552d7b97299c17554491
Payton Banks <payton.banks@gmail.com>
2020-09-27 14:33:17 -0400

    updates to styling and create

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,32 @@
-var connection = require("./connection.js");
+var connection = require("./connection");
 
 var orm = {
-    selectAll: function (cb) {
-        connection.query('SELECT * FROM burgers', (err, res) => {
+    all: function(cb) {
+        connection.query('SELECT * FROM ??', [burgers], function(err, result) {
             if (err) throw err;
+            console.log(burgers);
             cb(res);
         });
     },
 
-    create: function (name, cb) {
+    create: function(name, cb) {
         connection.query('INSERT INTO burgers SET ?', {
-          burger_name: name,
-          devoured: false
-        }, (err, res) => {
-            if (err) throw err;
-            cb(res);
-        });
+            burger_name: name,
+            devoured: false
+        },
+            function (err, res) {    
+                if (err) throw err;
+                cb(res);
+            });
     },
 
-    update: function (newId, cb) {
-        connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id: newId}], (err, res) => {
+    update: function(newId, cb) {
+        connection.query('UPDATE burgers SET ? WHERE ?', [req.body.burgers], 
+        function(err, res) {
             if (err) throw err;
             cb(res);
         });
-    },
+    }
 
 };
 
